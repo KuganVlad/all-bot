5d978589ac26e44e32b4a481c030bbd7272c11df
KuganVlad <vlad_kugan@hotmail.com>
2023-06-27 22:06:22 +0300

    transfer function in db_function, adding functionality

---
+++ db_function.py
@@ -0,0 +1,122 @@
+import sqlite3
+
+# Создание базы данных
+def create_db():
+    # Создаем подключение к базе данных
+    conn = sqlite3.connect('bot_database.db')
+    cursor = conn.cursor()
+
+    # Создаем таблицу users
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tg_user_id INTEGER UNIQUE,
+            tg_first_name TEXT,
+            tg_last_name TEXT,
+            tg_user_name TEXT,
+            first_name TEXT,
+            last_name TEXT,
+            second_name TEXT,
+            age INTEGER,
+            city TEXT,
+            phone_number TEXT,
+            casting_status TEXT,
+            casting_id INTEGER,
+            FOREIGN KEY (casting_id) REFERENCES castings (casting_id)
+        )
+    ''')
+
+    # Создаем таблицу admins
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS admins (
+            admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tg_admin_id INTEGER UNIQUE
+        )
+    ''')
+
+    # Создаем таблицу questions
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS questions (
+            id_questions INTEGER PRIMARY KEY AUTOINCREMENT,
+            question TEXT,
+            user_id INTEGER,
+            date_time_question TEXT,
+            answer TEXT,
+            admin_id INTEGER,
+            date_time_answer TEXT,
+            FOREIGN KEY (user_id) REFERENCES users (user_id),
+            FOREIGN KEY (admin_id) REFERENCES admins (admin_id)
+        )
+    ''')
+
+    # Создаем таблицу data
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS data (
+            data_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            data_name TEXT,
+            data_text TEXT
+        )
+    ''')
+
+    # Создаем таблицу casting
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS castings (
+            casting_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            casting_city TEXT,
+            casting_date TEXT,
+            casting_time TEXT,
+            casting_status INTEGER,
+            casting_counter INTEGER
+        )
+    ''')
+
+
+    # Создаем таблицу teacher & manager
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS teachers (
+            teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            teacher_name TEXT,
+            teacher_major TEXT,
+            teacher_city TEXT,
+            teacher_status INTEGER,
+            is_manager INTEGER
+        )
+    ''')
+
+    # Создаем таблицу event
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS events (
+            event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event_city TEXT,
+            event_date TEXT,
+            event_time TEXT,
+            event_name TEXT,
+            casting_status INTEGER
+        )
+    ''')
+
+    # Сохраняем изменения в базе данных
+    conn.commit()
+
+    # Закрываем подключение к базе данных
+    conn.close()
+
+#  Проверка администратора
+def is_admin_allowed(user_id):
+    conn = sqlite3.connect('bot_database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM admins WHERE tg_admin_id = ?', (user_id,))
+    user = cursor.fetchone()
+    conn.close()
+    return bool(user)
+
+# Проверка пользователя
+def is_user_allowed(user_id):
+    conn = sqlite3.connect('bot_database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE tg_user_id = ?', (user_id,))
+    user = cursor.fetchone()
+    conn.close()
+    return bool(user)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram.dispatcher.filters import Command
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import sqlite3
+import db_function
 
 
 config = configparser.ConfigParser()
@@ -19,86 +20,17 @@ bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-# Определяем состояния для FSM
+# Определяем состояния для вопроса
 class UserStates(StatesGroup):
     MAIN_MENU = State()
     ASKING_QUESTION = State()
     ANSWERING_QUESTION = State()
 
+# Определяем состояния для старта
 class StartStates(StatesGroup):
     WHAT_NAME = State()
     WHAT_AGE = State()
     WHAT_CITY = State()
-
-
-def create_db():
-    # Создаем подключение к базе данных
-    conn = sqlite3.connect('bot_database.db')
-    cursor = conn.cursor()
-
-    # Создаем таблицу users
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tg_user_id INTEGER UNIQUE,
-            tg_first_name TEXT,
-            tg_last_name TEXT,
-            tg_user_name TEXT,
-            first_name TEXT,
-            last_name TEXT,
-            second_name TEXT,
-            age INTEGER,
-            city TEXT,
-            phone_number TEXT
-        )
-    ''')
-
-    # Создаем таблицу admins
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS admins (
-            admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tg_admin_id INTEGER UNIQUE
-        )
-    ''')
-
-    # Создаем таблицу questions
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS questions (
-            id_questions INTEGER PRIMARY KEY AUTOINCREMENT,
-            question TEXT,
-            user_id INTEGER,
-            date_time_question TEXT,
-            answer TEXT,
-            admin_id INTEGER,
-            date_time_answer TEXT,
-            FOREIGN KEY (user_id) REFERENCES users (user_id),
-            FOREIGN KEY (admin_id) REFERENCES admins (admin_id)
-        )
-    ''')
-
-    # Создаем таблицу data
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS data (
-            data_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            data_name TEXT,
-            data_text TEXT
-        )
-    ''')
-
-    # Сохраняем изменения в базе данных
-    conn.commit()
-
-    # Закрываем подключение к базе данных
-    conn.close()
-
-def is_user_allowed(user_id):
-    conn = sqlite3.connect('bot.db')
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM users WHERE tg_user_id = ?', (user_id,))
-    user = cursor.fetchone()
-    conn.close()
-    return bool(user)
 
 
 
@@ -107,27 +39,143 @@ async def start(message: types.Message, state: FSMContext):
     # Очищаем состояние пользователя при старте
     await state.finish()
     user_id = message.from_user.id
-    if is_user_allowed(user_id):
+    if db_function.is_admin_allowed(user_id):
         await message.answer("Приветствую! админ")
+    elif db_function.is_user_allowed(user_id):
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ["О модельной школе", "Направления обучения", "Учебные дисциплины", "Преподаватели",
+               "События", "Запись на кастинг", "Задать вопрос?"]
+        keyboard.add(*buttons)
+        await message.answer("Выбери интересующий раздел", reply_markup=keyboard)
+        await UserStates.MAIN_MENU.set()
     else:
         # Информация для простох пользователей
         await message.answer("Приветствую! Давай дружить! Как тебя зовут?")
         await StartStates.WHAT_NAME.set()
 
-@dp.message_handler(text="Задать вопрос?", state=StartStates.WHAT_NAME)
+@dp.message_handler(Command("help"), state="*")
+async def start(message: types.Message, state: FSMContext):
+    await state.finish()
+    user_id = message.from_user.id
+    if db_function.is_user_allowed(user_id):
+        await message.answer("Описание бота для зарегистрированного пользователя")
+        await UserStates.MAIN_MENU.set()
+    else:
+        # Информация для простох пользователей
+        await message.answer("Описание бота для зарегистрированного пользователя")
+        await StartStates.WHAT_NAME.set()
+
+# Начала диалога для пользователя, запрос имени
+@dp.message_handler(state=StartStates.WHAT_NAME)
 async def ask_question(message: types.Message, state: FSMContext):
     first_name = message.text
-    last_name = None
-    second_name = None
     age = None
     city = None
-    # Устанавливаем состояние ASKING_QUESTION
+    # Сохраняем имя пользователя в состояние
+    await state.update_data(first_name=first_name)
     await message.answer("Сколько тебе лет?")
     await StartStates.WHAT_AGE.set()
 
+# запрос возраста
+@dp.message_handler(state=StartStates.WHAT_AGE)
+async def ask_question(message: types.Message, state: FSMContext):
+    # Получаем возраст пользователя
+    age = message.text
+    await message.answer("Отлично! А теперь введи свой город:")
+    # Устанавливаем состояние ASKING_CITY
+    await StartStates.WHAT_CITY.set()
+    # Сохраняем возраст пользователя в состояние
+    await state.update_data(age=age)
+
+# запрос города, запись первичной информации в базу данных
+@dp.message_handler(state=StartStates.WHAT_CITY)
+async def ask_question(message: types.Message, state: FSMContext):
+    # Получаем возраст пользователя
+    city = message.text
+    data = await state.get_data()
+    first_name = data.get("first_name")
+    age = data.get("age")
+
+    # Сохраняем данные пользователя в базу данных
+    conn = sqlite3.connect('bot_database.db')
+    cursor = conn.cursor()
+
+    # Проверяем, существует ли пользователь с данным tg_user_id
+    cursor.execute('SELECT tg_user_id FROM users WHERE tg_user_id = ?', (message.from_user.id,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        # Пользователь уже зарегистрирован, выполняем операцию обновления данных
+        cursor.execute('''
+            UPDATE users SET tg_first_name=?, tg_last_name=?, tg_user_name=?, first_name=?, last_name=?, second_name=?, age=?, city=?, phone_number=?, casting_status=?, casting_id=?
+            WHERE tg_user_id=?
+        ''', (message.from_user.first_name, message.from_user.last_name, message.from_user.username,
+              first_name, None, None, age, city, None, None, None, message.from_user.id))
+    else:
+        # Пользователь не зарегистрирован, выполняем операцию вставки
+        cursor.execute('''
+            INSERT INTO users (tg_user_id, tg_first_name, tg_last_name, tg_user_name, first_name, last_name, second_name, age, city, phone_number, casting_status, casting_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (message.from_user.id, message.from_user.first_name, message.from_user.last_name,
+              message.from_user.username, first_name, None, None, age, city, None, None, None))
+
+    conn.commit()
+    conn.close()
+
+    # Отправляем приветственное сообщение и главное меню
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["О модельной школе", "Направления обучения", "Учебные дисциплины", "Преподаватели",
+               "События", "Запись на кастинг", "Задать вопрос?"]
+    keyboard.add(*buttons)
+    await message.answer(f"Добро пожаловать в бот-помощник модельной школы All-models!\n"
+                         f"Здесь ты можешь ознакомиться с информацией о модельной школе, направлениях обучения,\n"
+                         f"учебных дисциплинах, преподавателях, предстоящих событиях, записаться на кастинг\n"
+                         f"или же задать интересующий тебя вопрос!", reply_markup=keyboard)
+
+    # Устанавливаем состояние MAIN_MENU
+    await UserStates.MAIN_MENU.set()
+
+# Функция возврата в главное меню
+@dp.message_handler(lambda message: message.text == "Вернуться в главное меню", state="*")
+async def return_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    user_id = message.from_user.id
+    if db_function.is_user_allowed(user_id):
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ["О модельной школе", "Направления обучения", "Учебные дисциплины", "Преподаватели",
+               "События", "Запись на кастинг", "Задать вопрос?"]
+        keyboard.add(*buttons)
+        await message.answer("Выбери интересующий раздел", reply_markup=keyboard)
+        await UserStates.MAIN_MENU.set()
+    else:
+        await message.answer("Доступ закрыт.")
 
 
+@dp.message_handler()
+async def handle_button_click(message: types.Message):
+    user_id = message.from_user.id
+    if db_function.is_user_allowed(user_id):
+        if message.text == "О модельной школе":
+            pass
+        elif message.text == "Направления обучения":
+            pass
+        elif message.text == "Учебные дисциплины":
+            pass
+        elif message.text == "Направления обучения":
+            pass
+        elif message.text == "Преподаватели":
+            pass
+        elif message.text == "События":
+            pass
+        elif message.text == "Запись на кастинг":
+            pass
+        else:
+            await message.answer("Неизвестная команда")
+    else:
+        await message.answer("Для получения доступа к боту необходимо пройти процедуру регистрации")
 
+
+# Раздел задать вопрос
 @dp.message_handler(text="Задать вопрос?", state=UserStates.MAIN_MENU)
 async def ask_question(message: types.Message, state: FSMContext):
     # Устанавливаем состояние ASKING_QUESTION
@@ -169,13 +217,12 @@ async def answer_question_callback(query: types.CallbackQuery, state: FSMContext
     await query.message.answer("Введите ответ на вопрос пользователя:")
 
 
-
 @dp.message_handler(state=UserStates.ANSWERING_QUESTION)
-async def process_answer(message: types.Message, state: FSMContext):
+async def process_answer(query: types.Message, state: FSMContext):
     # Получаем идентификатор пользователя и ответ из состояния
     data = await state.get_data()
     user_id = data.get("user_id")
-    answer = message.text
+    answer = query.text
 
     # Отправляем ответ пользователю
     await bot.send_message(user_id, f"Ответ от администратора:\n\n{answer}")
@@ -183,9 +230,11 @@ async def process_answer(message: types.Message, state: FSMContext):
     # Устанавливаем состояние MAIN_MENU
     await UserStates.MAIN_MENU.set()
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add("Задать вопрос?", "Вернуться в главное меню")
-    await message.answer("Ответ успешно отправлен пользователю.", reply_markup=keyboard)
+    keyboard.add("Преподаватели", "События", "Запись на кастинг")
+    await query.answer("Ответ успешно отправлен пользователю.")
+
+
 
 if __name__ == '__main__':
-    create_db()
+    db_function.create_db()
     executor.start_polling(dp, skip_updates=True)
